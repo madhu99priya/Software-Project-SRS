6ecdea29252e2826f1d583d8bee42fc8945c5040
madhu99priya <157110469+madhu99priya@users.noreply.github.com>
2024-06-13 21:01:33 +0530

    plans sub completed

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Branding from '../Branding/Branding.jsx'
 import Reasons from '../Reasons/Reasons.jsx'
 import './Home.css'
+import Plans from '../Plans/Plans.jsx'
 
 const Home = () => {
   return (
     <div >
        <Branding />
        < Reasons />
+       < Plans />
     </div >
   )
 }
---
+++ frontend/src/components/Plans/Plans.jsx
@@ -0,0 +1,43 @@
+
+import React from 'react'
+import './Plans.css'
+import { plansData } from '../../../data/plansData.jsx'
+import whiteTick from '../../assets/whiteTick.png'
+import { Link } from 'react-router-dom'
+
+const Plans = () => {
+  return (
+    
+    <div className="plans-container">
+        <div className="programs_header" style={{gap:' 2.4rem'}}>
+            <span className='stroke-text'>LEVEL UP </span>
+            <span> YOUR JOURNEY</span>
+            <span className='stroke-text'>  WITH US </span>
+        </div>
+
+        <div className="plans">
+           { plansData.map((plan) => (
+            <div className="plan" key={plan.id} >
+                {plan.icon}
+                <span>{plan.name}</span>
+                <span>Rs. {plan.price}</span>
+
+                <div className="features">
+                    {plan.features.map((feature,index) =>(
+                        <div className="feature" key={index} >
+                            <span className='tic-img'><img src={whiteTick} alt="" /></span>
+                            <span >{feature}</span>
+                        </div>
+                    ))}
+                </div>
+                <Link to = '/paymentForm' className='button'>Register Now</Link>
+            </div>
+           ))}
+
+        </div>
+    </div>
+  )
+}
+
+export default Plans
+
