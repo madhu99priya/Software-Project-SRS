26579261ba24afa892314ada6683cab11df122ca
madhu99priya <157110469+madhu99priya@users.noreply.github.com>
2024-06-13 21:28:43 +0530

    Reasons styling changed
    sajana

--- frontend/src/components/Reasons/Reasons.jsx
+++ frontend/src/components/Reasons/Reasons.jsx
@@ -30,18 +30,18 @@ const Reasons = () => {
 
     <div className="reasons-container">
         <div className="header">
-            <h3>
-                <span>WHY</span> 
-                <span> CHOOSE US</span>
-            </h3>
-            <span>To deliver world class indoor sports experience to people with safe, high quality service and focusing on teaching kids how to move forward with sports-specific training to empower our community.</span>
+        <br></br>
+          <br></br>
+            <h1>
+                WHY <span className='highlight'> CHOOSE US</span>
+            </h1>
+            <p1>To deliver world class indoor sports experience to people with safe, <br></br>high quality service and focusing on teaching kids how to move forward with <br></br>sports-specific training to empower our community.</p1>
         </div>
 
         <div className="cards">
         {reasons.map((reason) => (
         <div key={reason.id} className="reason-card">
-          <h2>{reason.id}</h2>
-          <h3>{reason.title}</h3>
+          <h2>{reason.id}. {reason.title}</h2>
           <p>{reason.description}</p>
         </div>
       ))}
