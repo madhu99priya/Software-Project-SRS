66f63d10d8224f6ca1e8e8eb107a8423f2f743b7
Sajana4197 <158558553+Sajana4197@users.noreply.github.com>
2024-09-03 00:53:28 +0530

    contactus_css
    change the stylings of contact us form

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,49 +1,7 @@
-// import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-// import './Navbar.css';
-// import Logo from '../../assets/Logo2.png'
-
-// const Navbar = () => {
-//   const [scrolled, setScrolled] = useState(false);
-
-//   const handleScroll = () => {
-//     const offset = window.scrollY;
-//     if (offset > 200) {
-//       setScrolled(true);
-//     } else {
-//       setScrolled(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener('scroll', handleScroll);
-//     return () => window.removeEventListener('scroll', handleScroll);
-//   }, []);
-
-//   return (
-//     <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
-//       <div className="navbar-container">
-//         <Link to="/" className="logo"><img src={Logo} alt="" className='logo' /></Link>
-//         <div className="nav-menu">
-//           <Link to="/" className="nav-item">Home</Link>
-//           <Link to="/services" className="nav-item">Services</Link>
-//           <Link to="/packages" className="nav-item">Packages</Link>
-//           <Link to="/online-reservations" className="nav-item">Online Reservations</Link>
-//           <Link to="/contact" className="nav-item">Contact Us</Link>
-//           <Link  to= '/memberlogin' className='nav-item'>Members Area</Link>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
-
-import React, { useState, useEffect } from 'react';
-import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
-import './Navbar.css';
-import Logo from '../../assets/Logo2.png';
+import React, { useState, useEffect } from "react";
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
+import "./Navbar.css";
+import Logo from "../../assets/Logo2.png";
 import { MdAdminPanelSettings } from "react-icons/md";
 
 const Navbar = () => {
@@ -59,24 +17,63 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
+    <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
       <div className="navbar-container">
         <a href="/" className="logo" onClick={() => scroll.scrollToTop()}>
-          <img src={Logo} alt="Logo" className='logo' />
+          <img src={Logo} alt="Logo" className="logo" />
         </a>
         <div className="nav-menu">
-          <ScrollLink to="home" smooth={true} duration={500} className="nav-item">Home</ScrollLink>
-          <ScrollLink to="services" smooth={true} duration={500} className="nav-item">Services</ScrollLink>
-          <ScrollLink to="packages" smooth={true} duration={500} className="nav-item">Packages</ScrollLink>
-          <ScrollLink to="online-reservations" smooth={true} duration={500} className="nav-item">Online Reservations</ScrollLink>
-          <ScrollLink to="contact-container" smooth={true} duration={500} className="nav-item">Contact Us</ScrollLink>
-          <a href='/memberlogin' className='nav-item'>Member's Area</a>
-          <a href="/adminpanel" className='admin'><MdAdminPanelSettings size={33} color='black'/> </a>
+          <ScrollLink
+            to="home"
+            smooth={true}
+            duration={500}
+            className="nav-item"
+          >
+            Home
+          </ScrollLink>
+          <ScrollLink
+            to="services"
+            smooth={true}
+            duration={500}
+            className="nav-item"
+          >
+            Services
+          </ScrollLink>
+          <ScrollLink
+            to="packages"
+            smooth={true}
+            duration={500}
+            className="nav-item"
+          >
+            Packages
+          </ScrollLink>
+          <ScrollLink
+            to="online-reservations"
+            smooth={true}
+            duration={500}
+            className="nav-item"
+          >
+            Online Reservations
+          </ScrollLink>
+          <ScrollLink
+            to="contact-container"
+            smooth={true}
+            duration={500}
+            className="nav-item"
+          >
+            Contact Us
+          </ScrollLink>
+          <a href="/memberlogin" className="nav-item">
+            Member's Area
+          </a>
+          <a href="/adminpanel" className="admin">
+            <MdAdminPanelSettings size={33} color="black" />{" "}
+          </a>
         </div>
       </div>
     </nav>
@@ -84,4 +81,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
